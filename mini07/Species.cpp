cf063f5f5043320c1a4cf11a7c705a402a908b6d
Ján Horváth <xhorvat6@fi.muni.cz>
2016-11-17 21:01:09 +0100

    removed unnecessary "virtual"s in Species.cpp

--- mini07/Species.cpp
+++ mini07/Species.cpp
@@ -1,22 +1,22 @@
 #include "Species.hpp"
 
 class Charmander : public Species {
-  virtual unsigned int attack_power(unsigned int) const override { return 1; }
-  virtual unsigned int defense_power(unsigned int) const override { return 2; };
+  unsigned int attack_power(unsigned int) const override { return 1; }
+  unsigned int defense_power(unsigned int) const override { return 2; };
 };
 
 class Charmeleon : public Species {
-  virtual unsigned int attack_power(unsigned int level) const override {
+  unsigned int attack_power(unsigned int level) const override {
     return 1 + level;
   }
-  virtual unsigned int defense_power(unsigned int level) const override {
+  unsigned int defense_power(unsigned int level) const override {
     return 2 + level;
   }
 };
 
 class Charizard : public Species {
-  virtual unsigned int attack_power(unsigned int) const override { return 7; }
-  virtual unsigned int defense_power(unsigned int level) const override {
+  unsigned int attack_power(unsigned int) const override { return 7; }
+  unsigned int defense_power(unsigned int level) const override {
     return 10 * level;
   }
 };
