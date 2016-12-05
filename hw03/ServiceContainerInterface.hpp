f1819f30940682f80c2390e3e7034de491725f1b
Ján Horváth <xhorvat6@fi.muni.cz>
2016-12-05 19:22:31 +0100

    Upload new file

---
+++ hw03/ServiceContainerInterface.hpp
@@ -0,0 +1,18 @@
+#ifndef HW03_ACCOUNTING_SERVICE_CONTAINER_INTERFACE_H
+#define HW03_ACCOUNTING_SERVICE_CONTAINER_INTERFACE_H
+
+#include "InvoiceFacadeInterface.hpp"
+#include "SubjectsFacadeInterface.hpp"
+
+/**
+ * DO NOT MODIFY this file, Kontr will ignore the changes anyway.
+ */
+class ServiceContainerInterface {
+public:
+	virtual ~ServiceContainerInterface() = default;
+
+	virtual InvoiceFacadeInterface &getInvoiceFacade() = 0;
+	virtual SubjectsFacadeInterface &getSubjectsFacade() = 0;
+};
+
+#endif //HW03_ACCOUNTING_SERVICE_CONTAINER_INTERFACE_H
