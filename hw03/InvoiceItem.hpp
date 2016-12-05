c1a9b0c67590e987c4c983057ec55b3a6648ae20
Ján Horváth <xhorvat6@fi.muni.cz>
2016-12-05 19:21:28 +0100

    Upload new file

---
+++ hw03/InvoiceItem.hpp
@@ -0,0 +1,31 @@
+#ifndef HW03_ACCOUNTING_INVOICE_ITEM_H
+#define HW03_ACCOUNTING_INVOICE_ITEM_H
+
+#include <string>
+
+/**
+ * You can modify this file a little bit, but keep the provided constructor, it's neccesary.
+ */
+class InvoiceItem {
+	std::string title;
+	unsigned amount;
+	unsigned taxRate;
+
+public:
+	InvoiceItem(const std::string &title, unsigned amount, unsigned taxRate)
+	    : title(title), amount(amount), taxRate(taxRate) {}
+
+	const std::string &getTitle() const {
+		return title;
+	}
+
+	unsigned getAmount() const {
+		return amount;
+	}
+
+	unsigned getTaxRate() const {
+		return taxRate;
+	}
+};
+
+#endif //HW03_ACCOUNTING_INVOICE_ITEM_H
